370ee830fd7ff44916703140f50c304175c89ea4
mula5604 <emirmba@gmail.com>
2026-09-01 13:50:44 +0200

    made all tests for database_functions

--- test_database.py
+++ test_database.py
@@ -17,8 +17,39 @@ def test_execute_insert():
     assert "upper" in result.fetchall()[0]
 
 def test_get_id_or_raise():
+    #tests if correct things is being returned
     db = Database()
     db.execute(query="DELETE FROM splits;")
     split_id = db.insert("INSERT INTO splits (name) VALUES (?)", ("upper",))
     result = db.get_id_or_raise("splits", "name", "upper", "it aint there")
     assert result == split_id
+
+def test_insert():
+    #tests insert wrapper
+    db = Database()
+    db.execute(query="DELETE FROM splits;")
+    db.insert("INSERT INTO splits (name) VALUES (?)", ("upper",))
+    assert "upper" in db.execute(query="SELECT name from splits").fetchall()[0]
+
+def test_delete():
+    # tests delete wrapper
+    db = Database()
+    db.execute(query="DELETE FROM splits;")
+    db.insert("INSERT INTO splits (name) VALUES (?)", ("upper",))
+    db.delete("DELETE FROM splits WHERE name = ?", ("upper",))
+    assert db.execute(query="SELECT name FROM splits").fetchall() == []
+
+def test_fetchone():
+    # tests fetchone wrapper
+    db = Database()
+    db.execute(query="DELETE FROM splits;")
+    db.insert("INSERT INTO splits (name) VALUES (?)", ("upper",))
+    assert db.fetchone("SELECT name FROM splits WHERE name = ?", ("upper",))[0] == "upper"
+
+def test_fetchall():
+    # tests fetchall wrapper
+    db = Database()
+    db.execute(query="DELETE FROM splits;")
+    db.insert("INSERT INTO splits (name) VALUES (?)", ("upper",))
+    db.insert("INSERT INTO splits (name) VALUES (?)", ("lower",))
+    assert db.fetchall("SELECT name FROM splits") == [("upper",), ("lower",)]
