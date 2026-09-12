4ce651d083c605019c526e16e906c118f9392969
mula5604 <emirmba@gmail.com>
2026-09-12 22:55:20 +0200

    need to rewrite tests for models so they are independent of eachother

--- test_models.py
+++ test_models.py
@@ -1,3 +1,11 @@
+# NEED TO MAKE BETTER TEST DB
+# NEED TO MAKE TESTS HERE LESS REPETETIVE
+# THIS IS A TO DO
+#
+#
+
+
+
 import pytest
 from models import *
 
@@ -48,3 +56,34 @@ def test_get_logs_excercises():
 def test_delete_excercises():
     test_excercise.delete()
     assert test_excercise.name not in test_db.fetchall("select name from exercises;")
+
+# tests for workout table
+
+test_workout = Workout(test_db, test_split.id, "upper workout")
+
+def test_workout_save():
+    # tests if saving works
+    pass
+
+
+def test_get_by_name_workout():
+    # tests if getting by name works
+    #need to make a better test db
+    pass
+
+
+def test_get_exercises_workout():
+    # need to make workout exercises first
+    pass
+
+
+def test_get_logs_workout():
+    # need to make workout logs first
+    pass
+
+def test_delete_workout():
+    # tests if deleting works
+
+    test_workout.delete()
+
+    assert test_workout.name not in test_db.fetchall("SELECT name FROM workouts")
