dd79314db5c0fc3a368d3119978b71b9d26324d4
mula5604 <emirmba@gmail.com>
2026-09-16 23:54:02 +0200

    made the cli version somewhat well

--- main.py
+++ main.py
@@ -30,6 +30,9 @@ def main():
                 except sqlite3.IntegrityError:
                     print("split already exist")
                     continue
+                except ValueError:
+                    print("not found")
+                    continue
 
             case 2: # workout
                 try:
@@ -59,6 +62,9 @@ def main():
                 except sqlite3.IntegrityError:
                     print("workout already exist")
                     continue
+                except ValueError:
+                    print("not found")
+                    continue
 
 
             case 3: # Exercises
@@ -78,53 +84,59 @@ def main():
                 except sqlite3.IntegrityError:
                     print("excercise already exist")
                     continue
+                except ValueError:
+                    print("not found")
+                    continue
 
             case 4: # Workout Exercises
-                add_or_del = int(input("1 to delete a table 2 to add: "))
-                if add_or_del == 2:
-                    print(database.fetchall("SELECT name FROM splits;"))
-                    split_name = str(input("enter name of split: "))
-                    my_split = m.Split.get_by_name(database, split_name)
+                try:
+                    add_or_del = int(input("1 to delete a table 2 to add: "))
+                    if add_or_del == 2:
+                        print(database.fetchall("SELECT name FROM splits;"))
+                        split_name = str(input("enter name of split: "))
+                        my_split = m.Split.get_by_name(database, split_name)
 
-                    print(database.fetchall("SELECT name FROM workouts;"))
-                    workout_name = str(input("enter name of workout: "))
-                    my_workout = m.Workout.get_by_name(database, my_split.id ,workout_name)
+                        print(database.fetchall("SELECT name FROM workouts;"))
+                        workout_name = str(input("enter name of workout: "))
+                        my_workout = m.Workout.get_by_name(database, my_split.id ,workout_name)
 
-                    print(database.fetchall("SELECT name FROM exercises;"))
-                    excercise_name = str(input("enter name of exercise: "))
-                    my_excercise = m.Exercise.get_by_name(database, excercise_name)
+                        print(database.fetchall("SELECT name FROM exercises;"))
+                        excercise_name = str(input("enter name of exercise: "))
+                        my_excercise = m.Exercise.get_by_name(database, excercise_name)
 
-                    if my_workout == None or my_excercise == None:
-                        print("no such table")
-                        continue
-                    
-                    sets = int(input("how many sets: "))
+                        if my_workout == None or my_excercise == None:
+                            print("no such table")
+                            continue
+                        
+                        sets = int(input("how many sets: "))
 
-                    my_we = m.WorkoutExercise(database, my_workout.id, my_excercise.id, sets)
-                    my_we.save()
+                        my_we = m.WorkoutExercise(database, my_workout.id, my_excercise.id, sets)
+                        my_we.save()
 
-                    for i in range(sets):
-                        i += 1
-                        reps = int(input(f"how many reps in the {i} set: "))
-                        weight = int(input(f"weight moved in the {i} set: "))
-                        my_exercise_log = m.ExerciseLog(database, my_workout.id,my_excercise.id, weight, reps)
-                        my_exercise_log.save()
-                else:
-                    print(database.fetchall("SELECT name FROM workouts;"))
-                    workout_name = str(input("enter name of workout in which you want to delete something: "))
-                    my_workout = m.Workout.get_by_name(database, my_split.id ,workout_name)
+                        for i in range(sets):
+                            i += 1
+                            reps = int(input(f"how many reps in the {i} set: "))
+                            weight = int(input(f"weight moved in the {i} set: "))
+                            my_exercise_log = m.ExerciseLog(database, my_workout.id,my_excercise.id, weight, reps)
+                            my_exercise_log.save()
+                    else:
+                            print(database.fetchall("SELECT name FROM workouts;"))
+                            workout_name = str(input("enter name of workout in which you want to delete something: "))
+                            my_workout = m.Workout.get_by_name(database, my_split.id ,workout_name)
 
-                    print(database.fetchall("SELECT name FROM exercises;"))
-                    excercise_name = str(input("enter name of exercise in which you want to delete something: "))
-                    my_excercise = m.Exercise.get_by_name(database, excercise_name)
+                            print(database.fetchall("SELECT name FROM exercises;"))
+                            excercise_name = str(input("enter name of exercise in which you want to delete something: "))
+                            my_excercise = m.Exercise.get_by_name(database, excercise_name)
 
-                    result = database.fetchone("SELECT id FROM workout_exercises WHERE workout_id = ? AND exercise_id = ?",(my_workout.id, my_excercise.id))
+                            result = database.fetchone("SELECT id FROM workout_exercises WHERE workout_id = ? AND exercise_id = ?",(my_workout.id, my_excercise.id))
 
-                    if not result:
-                        raise ValueError("WorkoutExercise not found")
+                            if not result:
+                                raise ValueError("WorkoutExercise not found")
 
-                    database.delete("DELETE FROM workout_exercises WHERE id = ?",(result[0],))
-
+                            database.delete("DELETE FROM workout_exercises WHERE id = ?",(result[0],))
+                except ValueError:
+                    print("not found")
+                    continue
 
             case _: # quit
                 print("exiting...")
